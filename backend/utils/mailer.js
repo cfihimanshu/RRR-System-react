269553b914210ec7500f7a417c42d0ac675be8dd
cfhimanshu <cfi.himanshu@gmail.com>
2026-05-08 14:07:02 +0530

    Fix

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -2,21 +2,10 @@ const nodemailer = require('nodemailer');
 const fs = require('fs');
 const path = require('path');
 
-// FORCE RE-READ .env to bypass any cached injections
-const envPath = path.join(__dirname, '../.env');
-const envContent = fs.readFileSync(envPath, 'utf8');
-const envVars = {};
-envContent.split('\n').forEach(line => {
-  const [key, ...valueParts] = line.split('=');
-  if (key && valueParts.length > 0) {
-    envVars[key.trim()] = valueParts.join('=').trim();
-  }
-});
+const smtpUser = process.env.SMTP_USER;
+const smtpPass = process.env.SMTP_PASS;
 
-const smtpUser = envVars['SMTP_USER'] || process.env.SMTP_USER;
-const smtpPass = envVars['SMTP_PASS'] || process.env.SMTP_PASS;
-
-console.log('Mailer Force-Read Sync:', { 
+console.log('Mailer Init:', { 
   user: smtpUser, 
   passStart: smtpPass ? smtpPass.substring(0, 2) + '...' : 'NONE'
 });
