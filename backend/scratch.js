7df9ff4f3df28e644d41d72d842abc1d9371c706
cfihimanshu <cfi.himanshu@gmail.com>
2026-06-08 10:42:28 +0530

    fixing data in progress report

--- backend/scratch.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-  const Progress = require('./models/Progress');
-  const doc = await Progress.findOne({ caseId: 'RRR-SF-2026-0069', stage: 'Settlement' }).lean();
-  console.log(JSON.stringify(doc.updates, null, 2));
-  process.exit(0);
-}).catch(console.error);
